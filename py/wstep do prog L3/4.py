86519916b4135c53a65e7b5519e079c7823cb74b
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2022-11-08 18:20:37 +0100

    L3 ex. 4

---
+++ py/wstep do prog L3/4.py
@@ -0,0 +1,23 @@
+print("Kolejne liczby")
+print()
+print("Mniejsza liczba całkowita to początek, większa to koniec.")
+
+a = int(input("Pierwsza liczba: "))
+b = int(input("Druga liczba: "))
+(start, stop) = (a, b)
+if start > stop:
+    (start, stop) = (stop, start)
+
+first = True
+while start <= stop:
+    if start % 2 == 1:
+        start += 1
+        continue
+
+    if first:
+        print(start, end="")
+        first = False
+    else:
+        print(f", {start}", end="")
+
+    start += 1
