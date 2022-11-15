0d4f8358c026475c74ac20778ee9c15318bddd0e
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2022-11-15 18:07:56 +0100

    L4 ex. d2

---
+++ py/pythonProject/d2.py
@@ -0,0 +1,18 @@
+print("Obliczanie kwadratów kolejnych liczb całkowitych")
+print()
+
+n = int(input("Podaj ilość liczb: "))
+if n < 0:
+    print("Nieprawidłowa ilość.")
+    exit(-1)
+
+l = [[], []]
+for i in range(0, n):
+    number = i + 1
+    l[0].insert(i, number)
+    l[1].insert(i, number ** 2)
+
+for i in range(0, len(l[0])):
+    number = l[0][i]
+    squared = l[1][i]
+    print(f"{number}² = {squared}")
