c4315a696e46dfe03487ee4019ce1f5f6feaf7df
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2022-11-15 18:15:50 +0100

    Use append instead of insert in L4 ex. d2, d3

--- py/pythonProject/d2.py
+++ py/pythonProject/d2.py
@@ -9,8 +9,8 @@ if n < 0:
 l = [[], []]
 for i in range(0, n):
     number = i + 1
-    l[0].insert(i, number)
-    l[1].insert(i, number ** 2)
+    l[0].append(number)
+    l[1].append(number ** 2)
 
 for i in range(0, len(l[0])):
     number = l[0][i]
--- py/pythonProject/d3.py
+++ py/pythonProject/d3.py
@@ -3,10 +3,10 @@ chessBoardSide = 8
 chessBoard = []
 rowStart = 0
 for rowIndex in range(0, chessBoardSide):
-    chessBoard.insert(rowIndex, [])
+    chessBoard.append([])
     content = rowStart
     for columnIndex in range(0, chessBoardSide):
-        chessBoard[rowIndex].insert(columnIndex, content)
+        chessBoard[rowIndex].append(content)
         content = 1 if content == 0 else 0
 
     rowStart = 1 if rowStart == 0 else 0
