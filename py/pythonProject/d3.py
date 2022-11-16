cec572563c3ee761ff947bd4b486f22be6b96ff6
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2022-11-16 06:03:50 +0100

    L4/d3: size sounds better

--- py/pythonProject/d3.py
+++ py/pythonProject/d3.py
@@ -1,11 +1,11 @@
-chessBoardSide = 8
+chessBoardSize = 8
 
 chessBoard = []
 rowStart = 0
-for rowIndex in range(0, chessBoardSide):
+for rowIndex in range(0, chessBoardSize):
     chessBoard.append([])
     content = rowStart
-    for columnIndex in range(0, chessBoardSide):
+    for columnIndex in range(0, chessBoardSize):
         chessBoard[rowIndex].append(content)
         content = 1 if content == 0 else 0
 
